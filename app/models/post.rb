b4c60df3582652236e86c173ff21cfe309c26a0b
Jessica <J_Saltz@hotmail.com>
2012-06-14 18:14:39 -0400

    added comment search

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,15 @@ class Post < ActiveRecord::Base
 
   def self.search(search)
    if search
-       #TODO Add Comments
-       find(:all, :conditions => ['body LIKE ? or email LIKE ?', "%#{search}%", "%#{search}%"])
+     begin
+       #Lookup on posts and comments, could have also used reg expressions in a loop
+       @results = Post.connection.execute("select distinct p.id from posts p left join comments c on p.id = c.post_id where p.email like '%"+search+"%' or p.body like '%"+search+"%' or c.email like '%"+search+"%' or c.body like '%"+search+"%'")
+       @ids = []
+       @results.each do |i|
+         @ids << i["id"]
+       end
+       find(:all, :conditions => ['id in (?)', @ids])
+     end
   else
     find(:all)
 
