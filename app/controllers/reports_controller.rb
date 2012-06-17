2ffe9bed571780b00b995828be985c195108e431
Rita Lui <ritalui@me.com>
2012-06-17 19:19:10 -0400

    fix unauthorized access to user reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,20 @@
 
 class ReportsController < ApplicationController
+  before_filter :admin_user
+
   def index
     @posts = Post.all.sort
     @users = User.all.sort_by {|user| -user.posts.length }
+  end
+
+  def admin_user
+    @user = self.current_user
+    if !@user
+      redirect_to signin_path, notice: "Please sign in."
+    else
+      if !@user.isAdmin?
+        redirect_to posts_path, notice: "Action not permitted"
+      end
+    end
   end
 end
