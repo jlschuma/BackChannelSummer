5723af54eafa7970437ac5c88dfd525b1f7be705
Rita Lui <ritalui@me.com>
2012-06-18 15:17:39 -0400

    add check to see if current_user is admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,7 @@ class UsersController < ApplicationController
   # GET /users/new.json
   def new
     @user = User.new
+    @user.isAdmin = false
 
     respond_to do |format|
       format.html # new.html.erb
